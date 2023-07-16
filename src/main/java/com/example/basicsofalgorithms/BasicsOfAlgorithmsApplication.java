4707831595f24cdc1c3592bc244ec31e00b22b7e
Karina <bagdasarova-k@inbox.ru>
2023-07-17 02:21:06 +0500

    Basics of algorithms

---
+++ src/main/java/com/example/basicsofalgorithms/BasicsOfAlgorithmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.basicsofalgorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicsOfAlgorithmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicsOfAlgorithmsApplication.class, args);
+    }
+
+}
